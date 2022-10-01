393b7e6415ad6e5ec67474140f5c0f32ba64f671
farhanazam41 <39867162+farhanazam41@users.noreply.github.com>
2022-10-01 13:19:50 -0700

    add signup and signin routes

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,29 +1,118 @@
 import User from "../models/user-model.js";
+import jwt from "jsonwebtoken";
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const signUp = (req, res) => { 
-    console.log('req body', req.body);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    const { name, email, password, phone } = req.body;
+export const signUp = (req, res) => {
+	const { name, email, password, phone } = req.body;
 
+	User.findOne({ email }).exec((err, user) => {
+		if (user) {
+			return res.status(400).json({
+				error: "Email is taken",
+			});
+		}
+
+		const token = jwt.sign(
+			{ name, email, password, phone },
+			process.env.JWT_ACCOUNT_ACTIVATION,
+			{ expiresIn: "10m" }
+		);
+
+		const emailData = {
+			from: process.env.EMAIL_FROM,
+			to: email,
+			subject: `Account activation link`,
+			html: `
+                <h1>Please use the following link to activate your account</h1>
+                <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+                <hr />
+                <p>This email may contain sensetive information</p>
+                <p>${process.env.CLIENT_URL}</p>
+            `,
+		};
+
+		sgMail
+			.send(emailData)
+			.then((sent) => {
+				console.log("SIGNUP EMAIL SENT", sent);
+				return res.json({
+					message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+				});
+			})
+			.catch((err) => {
+				// console.log('SIGNUP EMAIL SENT ERROR', err)
+				return res.json({
+					message: err.message,
+				});
+			});
+	});
+};
+
+export const accountActivation = (req, res) => {
+	const { token } = req.body;
+
+	if (token) {
+		jwt.verify(
+			token,
+			process.env.JWT_ACCOUNT_ACTIVATION,
+			 (err, decoded) => {
+				if (err) {
+					console.log("JWT VERIFY IN ACCOUNT ACTIVATION ERROR", err);
+					return res.status(401).json({
+						error: "Expired link. Signup again",
+					});
+				}
+
+				const { name, email, password, phone } = jwt.decode(token);
+
+				const user = new User({ name, email, password ,phone});
+
+				user.save((err, user) => {
+					if (err) {
+						console.log("SAVE USER IN ACCOUNT ACTIVATION ERROR", err);
+						return res.status(401).json({
+							error: "Error saving user in database. Try signup again",
+						});
+					}
+					return res.json({
+						message: "Signup success. Please signin.",
+					});
+				});
+			}
+		);
+	} else {
+		return res.json({
+			message: "Something went wrong. Try again.",
+		});
+	}
+};
+
+export const signIn = (req, res) => {
+    const { email, password } = req.body;
+    // check if user exist
     User.findOne({ email }).exec((err, user) => {
-        if (user) {
+        if (err || !user) {
             return res.status(400).json({
-                error: 'Email is taken'
+                error: 'User with that email does not exist. Please signup'
             });
         }
-    });
-    const newUser = new User({ name, email, password, phone });
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match'
+            });
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        const { _id, name, email, role } = user;
 
-    newUser.save((err, success) => {
-        if (err) {
-            console.log('SIGNUP ERROR', err);
-            return res.status(400).json({ 
-                error: err
-            });
-        }
-        console.log('SIGNUP SUCCESS', success);
-        res.status(201).json({
-            message: 'Signup success! Please signin.'
+        return res.json({
+            token,
+            user: { _id, name, email, role }
         });
     });
-}
+};
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,10 +1,12 @@
 import express from 'express';  
-import { signUp } from '../controllers/auth-controller.js';
-import { userSignupValidator } from '../validators/auth-validator.js';
+import { signUp,accountActivation, signIn } from '../controllers/auth-controller.js';
+import { userSignupValidator,userSigninValidator } from '../validators/auth-validator.js';
 import runValidation from '../validators/index.js';
 
 const router = express.Router();
 
 router.post("/signup",userSignupValidator,runValidation, signUp);
+router.post('/account-activation', accountActivation);
+router.post('/signin', userSigninValidator, runValidation, signIn);
 
 export default router;
